635593c2d9074cf96da69c46d89ce19797a6ef4b
Lada3310 <waterfowlwww@gmail.com>
2025-10-03 20:08:37 +0300

    Vladik hueta

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-first = int(input('enter 1 num'))
-second = int(input('enter 2 num'))
-action = str(input('enter your action'))
-
-if action == '+':
-    print(first + second)
-
-elif action == '-':    
-    print(first - second)
-
-elif action == '*':    
-    print(first * second)
-
-elif action == '/':    
-    print(first / second)
-
-else:
-    print('di nahui')
+print('di nahui')
